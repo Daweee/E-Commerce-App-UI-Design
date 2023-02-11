273a334fc5027fa64f0850c00029bb6983a1cace
Christian <christian.mandawe@usjr.edu.ph>
2023-02-12 04:48:07 +0800

    first

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:first_app/pages/CartPage.dart';
+import 'package:first_app/pages/Homepage.dart';
+import 'package:first_app/pages/ItemPage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      routes: {
+        "/": (context) => HomePage(),
+        "cartPage": (context) => CartPage(),
+        "itemPage": (context) => ItemPage(),
+      },
+    );
+  }
+}
